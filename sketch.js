786cd9eb7203d5265013059816c914a444480a65
mandraque005 <102252009+mandraque005@users.noreply.github.com>
2022-07-01 14:55:49 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,36 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-var engine, world;
-var canvas;
-var palyer, playerBase, playerArcher;
-var baseimage;
-
-function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+function preload()
+{
+	
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-
-  engine = Engine.create();
-  world = engine.world;
-  angleMode(DEGREES);
-
-  //criar corpo da base do jogador
-  Canvas = CreateCnavas(windowWidth,windowHeight)
-
-  engine=Engine.create()
-  world = engine.world
-
-  angleMode(DEGREES);
-
-  var options = {
-    isStatic: true 
-  };
-
-  playerBase = Bodies.rectangle(200,350,180,150, options);
-  World.add(world, playerBase);
-
-  player = Bodies.rectangle(250,playerBase.positioon.y-160,50,180,options)
-  //criar corpo do jogador
+	createCanvas(800, 700);
 
 
+	engine = Engine.create();
+	world = engine.world;
 
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background(backgroundImg);
-
-  //exibir a imagem do jogador usando a função image()
-   Engine.update(engine);
-   Image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-   Image(playerimage,player.position.x,player.position.y,50,180)
-
-  //exibir a imagem da base do jogador usando a função image()
-
-
-  Engine.update(engine);
-
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("ARQUEIRO ÉPICO", width / 2, 100);
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
 }
+
+
+
